226a57a86c61d2bb9a81d83c3ba3aa13e528a39e
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-12-04 21:59:11 +0100

    feat: create signup function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class SecurityController extends AbstractController
 {
@@ -24,6 +29,88 @@ class SecurityController extends AbstractController
             'controller_name' => 'SecurityController',
             'last_username' => $lastUsername,
             'error' => $error
+        ]);
+    }
+
+    #[Route('/signup', name: 'app_signup', methods: ['GET', 'POST'])]
+    public function registration(Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $passwordEncoder): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $pseudo = $form->get('username')->getData();
+            $email = $form->get('email')->getData();
+
+            $existingUser = $manager->getRepository(User::class)->findOneBy(['username' => $pseudo]);
+
+            if ($existingUser) {
+                $this->addFlash(
+                    'error',
+                    'Username already exists.'
+                );
+                return $this->render('security/signup.html.twig', [
+                    'form' => $form->createView()
+                ]);
+            }
+
+            $existingUser = $manager->getRepository(User::class)->findOneBy(['email' => $email]);
+
+            if ($existingUser) {
+                $this->addFlash(
+                    'error',
+                    'Email already exists.'
+                );
+                return $this->render('security/signup.html.twig', [
+                    'form' => $form->createView()
+                ]);
+            }
+
+            if ($form['password']['first']->getData() !== $form['password']['second']->getData()) {
+                $this->addFlash(
+                    'error',
+                    'Passwords do not match.'
+                );
+                return $this->render('security/signup.html.twig', [
+                    'form' => $form->createView()
+                ]);
+            }
+
+            if ($form->isValid()) {
+                $user = $form->getData();
+
+                // Hash the password
+                $user->setPassword(
+                    $passwordEncoder->hashPassword(
+                        $user,
+                        $form->get('password')->getData()
+                    )
+                );
+
+                if (empty($user->getUsername())) {
+                    $user->setPseudo($user->getFirstname());
+                }
+
+                $this->addFlash(
+                    'success',
+                    'Your account has been created. You can now log in.'
+                );
+
+                $manager->persist($user);
+                $manager->flush();
+
+                return $this->redirectToRoute('app_login');
+            }
+        }
+
+        return $this->render('security/signup.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -4,21 +4,23 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('password', RepeatedType::class)
             ->add('firstname')
             ->add('lastname')
             ->add('username')
             ->add('profile_picture')
+            ->add('submit', SubmitType::class)
         ;
     }
 
