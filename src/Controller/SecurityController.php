daa0ed07a30d9f060e789a7c6100bc2b06c14512
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-12-07 18:44:51 +0100

    feat: create logout function

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -113,4 +113,10 @@ class SecurityController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout()
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
 }
