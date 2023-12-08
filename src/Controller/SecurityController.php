781a415861e27f505615b9d90158c5b64da92d72
Mathieu Le Puil <lepuilmathieu@gmail.com>
2023-12-08 18:59:32 +0100

    feat: connect to google account

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -299,4 +299,61 @@ class SecurityController extends AbstractController
             var_dump($e->getMessage()); die;
         }
     }
+
+    #[Route('/login/google', name: 'google_connect', methods: ['GET'])]
+    public function googleConnect(ClientRegistry $clientRegistry): Response
+    {
+        $client = $clientRegistry->getClient('google');
+        return $client->redirect(['email', 'profile']);
+    }
+
+    #[Route('/login/google/check', name: 'google_connect_check', methods: ['GET'])]
+    public function googleConnectCheck(ClientRegistry $clientRegistry, EntityManagerInterface $em, TokenStorageInterface $tokenStorage, SessionInterface $session): Response
+    {
+        $client = $clientRegistry->getClient('google');
+
+        try {
+            $user = $client->fetchUser();
+
+            $userData = $user->toArray();
+
+            $fullName = $userData['name'];
+            $nameParts = explode(' ', $fullName);
+
+            $firstName = array_shift($nameParts);
+            $lastName = implode(' ', $nameParts);
+
+            $userRepository = $em->getRepository(User::class);
+            $existingUser = $userRepository->findOneBy(['email' => $userData['email']]);
+
+            if($existingUser) {
+                $existingUser->setFirstname($firstName);
+                $existingUser->setLastname($lastName);
+                $newUser = $existingUser;
+            } else {
+                $newUser = new User();
+                $newUser->setUsername($userData['given_name']);
+                $newUser->setEmail($userData['email']);
+                $newUser->setOauth('google');
+                $newUser->setRoles(['ROLE_USER']);
+                $newUser->setProfilePicture($userData['picture']);
+                $newUser->setPassword('google');
+                $newUser->setFirstname($firstName);
+                $newUser->setLastname($lastName);
+            }
+
+            $em->persist($newUser);
+            $em->flush();
+
+            $token = new UsernamePasswordToken($newUser, 'main', $newUser->getRoles());
+
+            $tokenStorage->setToken($token);
+            $session->set('_security_main', serialize($token));
+
+            return $this->redirectToRoute('app_home');
+
+        } catch (IdentityProviderException $e) {
+            var_dump($e->getMessage()); die;
+        }
+    }
 }
